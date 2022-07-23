862d683ec6664f0ed20fa95ec78ee2839f174100
sonyshaik786 <sksony.pnr@gmail.com>
2022-07-23 21:11:38 +0530

    4thdatdivya

--- src/JavaPrgms1/Palindromeno.java
+++ src/JavaPrgms1/Palindromeno.java
@@ -31,5 +31,6 @@ public static void main(String[] args)
 	System.out.println("next day divya code");
 	System.out.println("3rd day sonys code");
 	System.out.println("3rd day of divyas code");
+	System.out.println("4th day of divyas");
 	}
 }
